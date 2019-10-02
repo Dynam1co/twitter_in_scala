9eb06b4077b630b145bd61ad2cabdf55f67d7f37
Francisco Javier Asensi Benito <asensi.benito@gmail.com>
2019-10-02 22:40:09 +0200

    Another try

--- build.sbt
+++ build.sbt
@@ -6,3 +6,6 @@ scalaVersion := "2.12.10"
 
 libraryDependencies += "org.apache.spark" % "spark-core_2.12" % "2.4.4"
 libraryDependencies += "org.apache.spark" % "spark-streaming_2.12" % "2.4.4"
+libraryDependencies += "org.twitter4j" % "twitter4j-core" % "3.0.3"
+libraryDependencies += "org.twitter4j" % "twitter4j-stream" % "3.0.3"
+libraryDependencies += "org.apache.bahir" % "spark-streaming-twitter_2.12" % "2.4.0"
--- src/main/scala/test.scala
+++ src/main/scala/test.scala
@@ -1,16 +1,39 @@
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.Seconds
+import twitter4j.conf.ConfigurationBuilder
+import twitter4j.auth.{AccessToken, OAuthAuthorization}
+import twitter4j.{Status, TwitterFactory}
+import org.apache.spark.streaming.twitter.TwitterUtils
+import org.apache.spark.streaming._
 
 object test {
     def main(args: Array[String]): Unit = {
-        val conf = new SparkConf()
-        conf.setAppName("LearnScalaSpark")
-        conf.setMaster("local")
+        val appName = "TwitterData"
+        //create context
+        val scparkConf = new SparkConf().setAppName(appName)
+        scparkConf.setMaster("local")
 
-        val sc = new SparkContext(conf)
+        val ssc = new StreamingContext(scparkConf, Seconds(10))
 
-        sc.setLogLevel("OFF")
+        // values of Twitter API.
+        val consumerKey = "" // Your consumerKey
+        val consumerSecret = "" // your API secret
+        val accessToken ="" // your access token
+        val accessTokenSecret = "" // your token secret
 
-        val helloWorldString = "Hello World!"
-        print(helloWorldString)
+        val twitter = new TwitterFactory().getInstance()
+        twitter.setOAuthConsumer(consumerKey, consumerSecret)
+        twitter.setOAuthAccessToken(new AccessToken(accessToken, accessTokenSecret))
+
+        val stream = TwitterUtils.createStream(ssc, None).window(Seconds(20))
+        val hashTags = stream.flatMap(status => status.getText.split(" ").filter(_.startsWith("#")))
+
+        hashTags.saveAsTextFiles("./Tweets")
+        //println(hashTags)
+
+        ssc.start()
+        ssc.awaitTermination()
     }
 }
